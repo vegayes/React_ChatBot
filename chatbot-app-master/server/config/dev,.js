39be76397170dc25fb03cd02ee07195bc1948211
vegayes <vegayes@naber.com>
2024-02-15 23:52:13 +0900

    eunseo

---
+++ chatbot-app-master/server/config/dev,.js
@@ -0,0 +1,8 @@
+modle.exports = {
+    googleProjectID: '',
+    dialogFlowSessionID: 'bot-session',
+    dialogFlowSessionLanguageCode: 'en-US',
+    googleClientEmail:'',
+    googlePrivateKey: '',
+    mongoURI:'',
+}
